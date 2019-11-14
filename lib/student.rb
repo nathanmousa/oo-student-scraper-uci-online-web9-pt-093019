21fa8a0cd37ae6a4102956ab301e48806776b3c3
Nathan Mousa <nathanmousa1@gmail.com>
2019-11-14 19:27:46 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -10,15 +10,15 @@ class Student
   end
 
   def self.create_from_collection(students_array)
-    self.send("twitter=", attributes_hash[:twitter])
-    self.send("linkedin=", attributes_hash[:linkedin])
-    self.send("github=", attributes_hash[:github])
-    self.send("blog=", attributes_hash[:blog])
-    self.send("profile_quote=", attributes_hash[:profile_quote])
-    self.send("bio=", attributes_hash[:bio])
+    self.send("twitter=", student_hash[:twitter])
+    self.send("linkedin=", student_hash[:linkedin])
+    self.send("github=", student_hash[:github])
+    self.send("blog=", student_hash[:blog])
+    self.send("profile_quote=", student_hash[:profile_quote])
+    self.send("bio=", student_hash[:bio])
   end
 
-  def add_student_attributes(attributes_hash)
+  def add_student_attributes(student_hash)
     
   end
 
